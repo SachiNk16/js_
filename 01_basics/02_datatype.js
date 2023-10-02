7d8127a01405a2843e9ab2926acdf702524af32a
Sachin Mondal <145895029+SachiNk16@users.noreply.github.com>
2023-10-02 19:11:24 +0000

    container config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use Strict"; //treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+
+
+
+let Name="sachin"
+let age =20
+let isLoggedin = false
+let state;
+
+
+
+
+//number = 2 to the power of 53
+//bigint
+//string = ""
+//boolean= true/false
+//null = standalone value
+// undefine =
+// symbol= unique 
+
+
+
+//abject
+
+console.log(typeof"sachin");
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefineds
