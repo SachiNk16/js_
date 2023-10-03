0fe8dff42f8802533859a739b167f6ece2911094
Sachin Mondal <145895029+SachiNk16@users.noreply.github.com>
2023-10-03 17:48:03 +0000

    conversion of datatype

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -5,12 +5,12 @@
  //let score=undefined
 let score="Sachin"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber =Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //33 = 33
 //"33abc"= NaN
@@ -20,9 +20,35 @@ console.log(valueInNumber);
 let isLoggedIn =1
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1= true;  0= false
 //" " =false
 
 //"sachin"= true
+
+
+
+
+/******************************  OPERATION **************************** */
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+//console.log(2+2);
+
+
+let str1="sachin"
+let str2=" kumar"
+let str3=str1+str2
+//console.log(str3);
+
+
+// console.log("1" +2);
+// console.log(1+ "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(true)
+console.log(+true);
+
